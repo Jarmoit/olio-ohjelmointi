02ba4c0742db49e968dd6cd85549e172c02a023d
jarmoit <jmarjakangas@gmail.com>
2024-12-09 11:54:01 +0200

    h6a valmis

---
+++ h6a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    MyClass testi;
+    testi.raiseMySignal();
+
+    return 0;
+}
